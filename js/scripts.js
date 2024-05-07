78600f300da1e81be32712ae62a3b17ee2a2e0f1
cwh0204 <48270074+cwh0204@users.noreply.github.com>
2024-05-07 18:33:13 +0900

    수정
    수정

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,15 @@
 //
 // Scripts
 // 
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
 
 window.addEventListener('DOMContentLoaded', event => {
 
